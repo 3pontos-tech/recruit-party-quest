feba492c5b51af8a9500531c82f54084b4298492
danielhe4rt <danielhe4rt@gmail.com>
2026-01-15 17:23:26 -0300

    feat(applications): initialize applications module

---
+++ app-modules/applications/src/ApplicationsServiceProvider.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace He4rt\Applications;
+
+use Illuminate\Support\ServiceProvider;
+
+class ApplicationsServiceProvider extends ServiceProvider
+{
+    public function register(): void {}
+
+    public function boot(): void {}
+}
