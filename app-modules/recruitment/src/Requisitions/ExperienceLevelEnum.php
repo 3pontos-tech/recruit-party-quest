a373a2d29f21f291c34bd88b560b815ce495e9e4
Clintonrocha98 <clintongoncalvesrocha@gmail.com>
2026-01-15 15:04:19 -0300

    feat(recruitment): add ExperienceLevelEnum for job experience levels

---
+++ app-modules/recruitment/src/Requisitions/ExperienceLevelEnum.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace He4rt\Recruitment\Requisitions;
+
+use Filament\Support\Contracts\HasColor;
+use Filament\Support\Contracts\HasIcon;
+use Filament\Support\Contracts\HasLabel;
+use Filament\Support\Icons\Heroicon;
+
+enum ExperienceLevelEnum: string implements HasLabel, HasIcon, HasColor
+{
+    case Intern = 'intern';
+    case EntryLevel = 'entry_level';
+    case MidLevel = 'mid_level';
+    case Senior = 'senior';
+    case Lead = 'lead';
+    case Principal = 'principal';
+
+    public function getColor(): string
+    {
+        return match ($this) {
+            self::Intern => 'secondary',
+            self::EntryLevel => 'success',
+            self::MidLevel => 'warning',
+            self::Senior => 'danger',
+            self::Lead => 'primary',
+            self::Principal => 'info',
+        };
+    }
+
+    public function getIcon(): Heroicon
+    {
+        return match ($this) {
+            self::Intern => Heroicon::AcademicCap,
+            self::EntryLevel => Heroicon::User,
+            self::MidLevel => Heroicon::Briefcase,
+            self::Senior => Heroicon::Star,
+            self::Lead => Heroicon::UserGroup,
+            self::Principal => Heroicon::ShieldCheck,
+        };
+    }
+
+    public function getLabel(): string
+    {
+        return match ($this) {
+            self::Intern => 'Estágio',
+            self::EntryLevel => 'Júnior',
+            self::MidLevel => 'Pleno',
+            self::Senior => 'Sênior',
+            self::Lead => 'Lead',
+            self::Principal => 'Principal',
+        };
+    }
+}
