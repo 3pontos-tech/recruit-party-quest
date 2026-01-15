33a19a653fdc04bed33bb5e40e16eead1ee10b33
Clintonrocha98 <clintongoncalvesrocha@gmail.com>
2026-01-15 15:04:57 -0300

    feat(recruitment): add WorkArrangementEnum for job work arrangements

---
+++ app-modules/recruitment/src/Requisitions/WorkArrangementEnum.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace He4rt\Recruitment\Requisitions;
+
+use Filament\Support\Contracts\HasColor;
+use Filament\Support\Contracts\HasIcon;
+use Filament\Support\Contracts\HasLabel;
+use Filament\Support\Icons\Heroicon;
+
+enum WorkArrangementEnum: string implements HasLabel, HasIcon, HasColor
+{
+    case Remote = 'remote';
+    case Hybrid = 'hybrid';
+    case OnSite = 'on_site';
+
+    public function getColor(): string
+    {
+        return match ($this) {
+            self::Remote => 'info',
+            self::Hybrid => 'warning',
+            self::OnSite => 'primary',
+        };
+    }
+
+    public function getIcon(): Heroicon
+    {
+        return match ($this) {
+            self::Remote => Heroicon::ComputerDesktop,
+            self::Hybrid => Heroicon::GlobeAlt,
+            self::OnSite => Heroicon::MapPin,
+        };
+    }
+
+    public function getLabel(): string
+    {
+        return match ($this) {
+            self::Remote => 'Remoto',
+            self::Hybrid => 'Híbrido',
+            self::OnSite => 'Presencial',
+        };
+    }
+}
