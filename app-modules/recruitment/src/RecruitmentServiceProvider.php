2b99654099ecb6a4d4d65ee46e952f7364de7ef0
danielhe4rt <danielhe4rt@gmail.com>
2026-01-15 13:45:26 -0300

    feat(recruitment): init recruitment module

---
+++ app-modules/recruitment/src/RecruitmentServiceProvider.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace He4rt\Recruitment;
+
+use Illuminate\Support\ServiceProvider;
+
+class RecruitmentServiceProvider extends ServiceProvider
+{
+    public function register(): void {}
+
+    public function boot(): void {}
+}
