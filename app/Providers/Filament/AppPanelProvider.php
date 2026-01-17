58c28d7709beef85642904636201cf88a59b157c
danielhe4rt <danielhe4rt@gmail.com>
2026-01-17 16:04:14 -0300

    feat(app): enhance the base app panel provider

--- app/Providers/Filament/AppPanelProvider.php
+++ app/Providers/Filament/AppPanelProvider.php
@@ -8,11 +8,12 @@ use App\Enums\FilamentPanel;
 use App\Filament\Shared\Pages\LoginPage;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
-use Filament\Pages\Dashboard;
+use Filament\Navigation\NavigationItem;
 use Filament\Panel;
 use Filament\PanelProvider;
 use Filament\Support\Colors\Color;
 use Filament\Support\Enums\Width;
+use Filament\View\PanelsRenderHook;
 use He4rt\App\Filament\Pages\AppDashboard;
 use He4rt\App\RedirectIfOnboardingIncomplete;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
@@ -20,6 +21,7 @@ use Illuminate\Cookie\Middleware\EncryptCookies;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\StartSession;
+use Illuminate\Support\Facades\Blade;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
 use Jeffgreco13\FilamentBreezy\BreezyCore;
 
@@ -38,14 +40,55 @@ class AppPanelProvider extends PanelProvider
             ->maxContentWidth(Width::ScreenTwoExtraLarge)
             ->path($this->panelEnum->getPath())
             ->colors([
-                'primary' => Color::Amber,
+                'primary' => Color::Gray,
             ])
+            ->renderHook(PanelsRenderHook::SIDEBAR_NAV_END, fn () => Blade::render(<<<'BLADE'
+               @guest
+                    <div class="flex flex-col md:hidden mt-auto items-center space-y-4">
+                        <x-he4rt::button rel="no-opener no-referrer" href="/login" icon="heroicon-s-arrow-top-right-on-square" variant="outline">
+                            Acessar Plataforma
+                        </x-he4rt::button>
+                    </div>
+               @endguest
+            BLADE
+            ))
+            ->renderHook(PanelsRenderHook::TOPBAR_END, fn () => Blade::render(<<<'BLADE'
+               @guest
+                    <div class="hidden md:flex items-center space-x-4">
+                        <x-he4rt::button rel="no-opener no-referrer" href="/login" icon="heroicon-s-arrow-top-right-on-square" variant="outline">
+                            Acessar Plataforma
+                        </x-he4rt::button>
+                    </div>
+               @endguest
+            BLADE
+            ))
+            ->navigationItems([
+                NavigationItem::make('Home')
+                    ->url('#hero')
+                    ->sort(0),
+                NavigationItem::make('Nossos pilares')
+                    ->url('#foundations')
+                    ->sort(1),
+                NavigationItem::make('Projects')
+                    ->url('#projects')
+                    ->sort(3),
+                NavigationItem::make('Comunidade')
+                    ->url('#community')
+                    ->sort(4),
+                NavigationItem::make('Eventos')
+                    ->url('#events')
+                    ->sort(5),
+                NavigationItem::make('Contato')
+                    ->url('#contact')
+                    ->sort(6),
+            ])
+            ->viteTheme('app-modules/he4rt/resources/css/themes/3pontos/theme.css')
             ->discoverClusters(in: base_path('app-modules/panel-app/src/Filament/Clusters'), for: 'He4rt\\App\\Filament\\Clusters')
             ->discoverPages(in: base_path('app-modules/panel-app/src/Filament/Pages'), for: 'He4rt\\App\\Filament\\Pages')
             ->discoverResources(in: base_path('app-modules/panel-app/src/Filament/Resources'), for: 'He4rt\\App\\Filament\\Resources')
             ->discoverWidgets(in: base_path('app-modules/panel-app/src/Filament/Widgets'), for: 'He4rt\\App\\Filament\\Widgets')
             ->pages([
-                auth()->check() ? Dashboard::class : AppDashboard::class,
+                AppDashboard::class,
             ])
             ->plugins([
                 BreezyCore::make()
