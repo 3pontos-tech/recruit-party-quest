6ec2a67996783af9bcf172310b897cb2446d133b
danielhe4rt <danielhe4rt@gmail.com>
2026-01-19 13:26:38 -0300

    fix: vite theme loading

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,12 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/filament/admin/theme.css', 'resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/filament/admin/theme.css',
+                'app-modules/he4rt/resources/css/themes/3pontos/theme.css',
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
         tailwindcss(),
