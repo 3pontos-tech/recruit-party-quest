d2a7cbe67ac089fad3c97a69d198892fa3b820b5
kaster <diogokaster@gmail.com>
2026-01-22 13:29:22 -0300

    refactor: remove knockout question helper text across Blade components and refine modal styling for improved accessibility and visual consistency

--- app-modules/he4rt/resources/views/components/modal/index.blade.php
+++ app-modules/he4rt/resources/views/components/modal/index.blade.php
@@ -23,7 +23,7 @@
         x-transition:leave="duration-200 ease-in"
         x-transition:leave-start="opacity-100"
         x-transition:leave-end="opacity-0"
-        class="bg-elevation-03dp/75 fixed inset-0 transition-opacity"
+        class="bg-elevation-surface/64 fixed inset-0 backdrop-blur-[2px] transition-opacity"
     ></div>
 
     <div class="fixed inset-0 z-10 w-screen overflow-y-auto">
@@ -36,7 +36,7 @@
                 x-transition:leave="duration-200 ease-in"
                 x-transition:leave-start="translate-y-0 opacity-100 sm:scale-100"
                 x-transition:leave-end="translate-y-4 opacity-0 sm:translate-y-0 sm:scale-95"
-                class="{{ $maxWidthClass }} bg-elevation-01dp relative w-full transform overflow-hidden rounded-lg px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 sm:p-6"
+                class="{{ $maxWidthClass }} bg-elevation-02dp relative w-full transform overflow-hidden rounded-md px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 sm:p-6"
                 @click.away="{{ $show }} = false"
             >
                 @if (isset($title))
--- app-modules/screening/resources/views/components/questions/text.blade.php
+++ app-modules/screening/resources/views/components/questions/text.blade.php
@@ -21,12 +21,6 @@
                 <span class="text-helper-error">*</span>
             @endif
         </x-he4rt::heading>
-
-        @if ($question->is_knockout)
-            <x-he4rt::text class="text-helper-warning font-family-secondary shrink-0 self-start text-sm">
-                {{ __('screening::question_types.knockout_helper') }}
-            </x-he4rt::text>
-        @endif
     </div>
 
     @if ($isMultiline)
