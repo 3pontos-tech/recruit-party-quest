9045e012d42dd518f2d4021a593f862dd24ba49d
kaster <diogokaster@gmail.com>
2026-01-28 07:57:21 -0300

    feat: refactor team about component to improve links handling and conditional rendering

--- app-modules/panel-app/resources/views/components/team/about.blade.php
+++ app-modules/panel-app/resources/views/components/team/about.blade.php
@@ -19,44 +19,46 @@
             </x-he4rt::text>
         </div>
 
-        {{-- Divider --}}
-        <div class="border-outline-low my-2 w-full border-t"></div>
+        @php
+            $links = $team->links;
+        @endphp
 
-        {{-- Links List --}}
-        <div class="flex flex-col gap-1">
-            @php
-                $links = $team->links;
-            @endphp
+        @if ($links->isNotEmpty())
+            {{-- Divider --}}
+            <div class="border-outline-low my-2 w-full border-t"></div>
 
-            @foreach ($links as $link)
-                <a
-                    href="{{ $link['url']['en'] }}"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    class="group hover:bg-elevation-02dp flex items-center justify-between rounded-lg p-3 transition-colors duration-200"
-                >
-                    <div class="flex items-center gap-3">
-                        <div class="text-icon-medium transition-colors">
-                            <x-he4rt::icon :icon="$link['icon']" />
+            {{-- Links List --}}
+            <div class="flex flex-col gap-1">
+                @foreach ($links as $link)
+                    <a
+                        href="{{ $link->url }}"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        class="group hover:bg-elevation-02dp flex items-center justify-between rounded-lg p-3 transition-colors duration-200"
+                    >
+                        <div class="flex items-center gap-3">
+                            <div class="text-icon-medium transition-colors">
+                                <x-he4rt::icon :icon="$link->icon" />
+                            </div>
+                            <x-he4rt::text size="sm" class="text-text-high font-medium transition-colors">
+                                {{ $link->name }}
+                            </x-he4rt::text>
                         </div>
-                        <x-he4rt::text size="sm" class="text-text-high font-medium transition-colors">
-                            {{ $link['name']['en'] }}
-                        </x-he4rt::text>
-                    </div>
 
-                    <div class="flex items-center gap-2">
-                        <span
-                            class="text-text-low group-hover:text-text-medium text-xs font-semibold transition-colors"
-                        >
-                            Access
-                        </span>
-                        <x-he4rt::icon icon="heroicon-m-chevron-right" size="sm" class="text-icon-low" />
-                    </div>
-                </a>
-                @if (! $loop->last)
-                    <div class="border-outline-low/30 mx-3 border-t"></div>
-                @endif
-            @endforeach
-        </div>
+                        <div class="flex items-center gap-2">
+                            <span
+                                class="text-text-low group-hover:text-text-medium text-xs font-semibold transition-colors"
+                            >
+                                Access
+                            </span>
+                            <x-he4rt::icon icon="heroicon-m-chevron-right" size="sm" class="text-icon-low" />
+                        </div>
+                    </a>
+                    @if (! $loop->last)
+                        <div class="border-outline-low/30 mx-3 border-t"></div>
+                    @endif
+                @endforeach
+            </div>
+        @endif
     </div>
 </x-he4rt::card>
