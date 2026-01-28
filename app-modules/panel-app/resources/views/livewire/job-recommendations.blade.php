41af858695778b0f2cfe29d5b07ba58f18426bf5
kaster <diogokaster@gmail.com>
2026-01-28 18:11:50 -0300

    feat: add spin animation to image in hero section

--- app-modules/panel-app/resources/views/livewire/job-recommendations.blade.php
+++ app-modules/panel-app/resources/views/livewire/job-recommendations.blade.php
@@ -30,7 +30,11 @@
 
     <div class="relative grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
         <div class="absolute right-0 -z-10">
-            <img src="{{ asset('images/3pontos/logo-rounded.svg') }}" alt="" class="h-auto w-full translate-x-[60%]" />
+            <img
+                src="{{ asset('images/3pontos/logo-rounded.svg') }}"
+                alt=""
+                class="spin-slow h-auto w-full translate-x-[60%]"
+            />
         </div>
 
         @forelse ($this->jobs as $job)
