e338b98725617ed3f8c43dd8928a9ba1e229f0bd
kaster <diogokaster@gmail.com>
2026-02-02 13:12:19 -0300

    feat: refine app-login-page with responsive layout adjustments for social login options

--- app-modules/panel-app/resources/views/filament/pages/app-login-page.blade.php
+++ app-modules/panel-app/resources/views/filament/pages/app-login-page.blade.php
@@ -49,7 +49,7 @@
             <div class="flex flex-col gap-8">
                 <x-he4rt::text class="text-center">Voce também pode fazer login utilizando:</x-he4rt::text>
 
-                <div class="grid grid-cols-1 gap-8 lg:grid-cols-3">
+                <div class="hidden grid-cols-3 gap-8 md:grid">
                     <x-he4rt::button icon="fab-google" variant="outline" class="px-1 font-bold">
                         Entrar com google
                     </x-he4rt::button>
@@ -63,7 +63,13 @@
                     </x-he4rt::button>
                 </div>
 
-                <div class="flex items-center justify-center gap-1">
+                <div class="flex items-center justify-center gap-8 md:hidden">
+                    <x-he4rt::button icon="fab-google" variant="outline" size="lg" />
+                    <x-he4rt::button icon="fab-github" variant="outline" size="lg" />
+                    <x-he4rt::button icon="fab-linkedin" variant="outline" size="lg" />
+                </div>
+
+                <div class="flex flex-col items-center justify-center gap-1 md:flex-row">
                     <x-he4rt::text>Não possui uma conta?</x-he4rt::text>
                     <a class="group" href="{{ route('filament.app.auth.register') }}">
                         <x-he4rt::text
