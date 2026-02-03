fb1085ea8683d0b636de67fa921dff489596e537
kaster <diogokaster@gmail.com>
2026-02-03 08:28:17 -0300

    feat: add decorative hourglass image to Filament app page

--- app-modules/panel-app/resources/views/filament/app.blade.php
+++ app-modules/panel-app/resources/views/filament/app.blade.php
@@ -1,4 +1,12 @@
 <filament::page class="relative" full-height="true">
+    <div class="pointer-events-none absolute top-0 left-0 max-w-7xl">
+        <img
+            src="{{ asset('images/3pontos/hourglass.svg') }}"
+            alt=""
+            class="h-auto w-full -translate-x-1/3 -translate-y-1/3"
+        />
+    </div>
+
     <section class="relative overflow-hidden py-6 md:py-10">
         <div class="relative mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
             <div class="flex flex-col justify-between gap-4 md:flex-row md:items-center">
