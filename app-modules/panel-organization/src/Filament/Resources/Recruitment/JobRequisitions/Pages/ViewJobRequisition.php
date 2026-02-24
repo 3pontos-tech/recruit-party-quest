febae8a6dedd4fb64d027f8b3ab039da3513c2ce
kaster <diogokaster@gmail.com>
2026-02-24 08:25:42 -0300

    feat: add ViewJobRequisition page and JobRequisitionInfolist schema to recruitment module

---
+++ app-modules/panel-organization/src/Filament/Resources/Recruitment/JobRequisitions/Pages/ViewJobRequisition.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace He4rt\Organization\Filament\Resources\Recruitment\JobRequisitions\Pages;
+
+use Filament\Resources\Pages\ViewRecord;
+use Filament\Schemas\Schema;
+use He4rt\Organization\Filament\Resources\Recruitment\JobRequisitions\JobRequisitionResource;
+use He4rt\Organization\Filament\Resources\Recruitment\JobRequisitions\Schemas\JobRequisitionInfolist;
+use He4rt\Recruitment\Requisitions\Models\JobRequisition;
+use Illuminate\Contracts\Support\Htmlable;
+
+class ViewJobRequisition extends ViewRecord
+{
+    protected static string $resource = JobRequisitionResource::class;
+
+    public function getTitle(): string|Htmlable
+    {
+        /** @var JobRequisition $record */
+        $record = $this->getRecord();
+
+        return $record->post->title;
+    }
+
+    public function infolist(Schema $schema): Schema
+    {
+        return JobRequisitionInfolist::configure($schema);
+    }
+}
