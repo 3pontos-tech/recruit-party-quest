68c5257f2d1afff616e8bd88655ebeae940f2c81
kaster <diogokaster@gmail.com>
2026-03-03 11:00:21 -0300

    refactor: reorder imports and add docblock for TeamMember properties

--- app-modules/teams/src/TeamMember.php
+++ app-modules/teams/src/TeamMember.php
@@ -8,6 +8,10 @@ use Illuminate\Database\Eloquent\Attributes\ObservedBy;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\Pivot;
 
+/**
+ * @property string $team_id
+ * @property string $user_id
+ */
 #[ObservedBy(TeamMemberObserver::class)]
 class TeamMember extends Pivot
 {
