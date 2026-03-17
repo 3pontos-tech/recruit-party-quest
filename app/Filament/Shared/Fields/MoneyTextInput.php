3e35c965660b1d772b9b082f8abbd29df0cef881
RichardGL11 <richardgreghilammenda@hotmail.com>
2026-03-17 15:11:56 -0300

    wip

---
+++ app/Filament/Shared/Fields/MoneyTextInput.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Filament\Shared\Fields;
+
+use Filament\Forms\Components\Field;
+
+class MoneyTextInput extends Field
+{
+    protected string $view = 'filament.forms.components.money-text-input';
+}
