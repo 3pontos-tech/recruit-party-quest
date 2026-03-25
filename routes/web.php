2ac3f3ac1a3c8b7c3382e07f60a7af8a47ebd27b
danielhe4rt <danielhe4rt@gmail.com>
2026-03-25 11:53:07 -0300

    fix: routing

--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,10 @@
 <?php
 
 declare(strict_types=1);
+
+use He4rt\Recruitment\Requisitions\Models\JobRequisition;
+use Illuminate\Support\Facades\Route;
+
+// Um abraço! (Big hug)
+// Claude TODO: remove this in two months from this commit.
+Route::get('/job-requisitions/{record:id}', fn (JobRequisition $record) => redirect()->to('/vagas/'.$record->post->slug));
