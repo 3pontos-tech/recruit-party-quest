7ad96fcdb1296aa129cf7aed26dab3c8e2ae266e
Clintonrocha98 <clintongoncalvesrocha@gmail.com>
2026-03-27 12:20:09 -0300

    wip

--- app-modules/panel-app/resources/views/components/team/about.blade.php
+++ app-modules/panel-app/resources/views/components/team/about.blade.php
@@ -12,13 +12,11 @@
         {{-- Header Section --}}
         <div class="flex flex-col gap-2">
             <div class="flex items-center gap-3">
-                @if ($team->getFirstMediaUrl('logo'))
-                    <img
-                        src="{{ $team->getFirstMediaUrl('logo', 'thumb') }}"
-                        alt="{{ $team->name }}"
-                        class="h-10 w-10 rounded-md object-cover"
-                    />
-                @endif
+                <img
+                    src="{{ $team->getFirstMediaUrl('logo', 'thumb') ?: asset('images/3pontos/logo-chain-white.png') }}"
+                    alt="{{ $team->name }}"
+                    class="h-10 w-10 rounded-md object-cover"
+                />
 
                 <x-he4rt::heading level="3" size="md" class="text-text-high">
                     {{ __('panel-app::filament.team.about.heading', ['name' => $team->name]) }}
